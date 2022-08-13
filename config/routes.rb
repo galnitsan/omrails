2660b99bc10ea60dcb16bc8b47356b6ed3836daf
Gal Nitsan <galnitsan@hotmail.com>
2022-08-13 16:39:20 +0300

    fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get 'signin' => 'devise/sessions#new'
+    delete "signout" => 'devise/sessions#destroy'
+    get 'signup' => 'devise/registrations#new'
+  end
+  
   get 'feed', to: 'feed#show'
   #get 'users/:username', to: 'users#show', as: 'user'
   resources :users, only: :show, param: :username do
@@ -12,13 +20,7 @@ Rails.application.routes.draw do
   resources :tweets do
       resources :comments
   end
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-    get 'signin' => 'devise/sessions#new'
-    delete "signout" => 'devise/sessions#destroy'
-    get 'signup' => 'devise/registrations#new'
-  end
+  
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact_us' => 'pages#contact_us'
