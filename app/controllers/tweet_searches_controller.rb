fffad6d46841af5ef17c5b5b32c7c5752c62ea60
Gal Nitsan <galnitsan@hotmail.com>
2022-08-30 20:33:13 +0300

    added ajax to tweet search with names

--- app/controllers/tweet_searches_controller.rb
+++ app/controllers/tweet_searches_controller.rb
@@ -3,13 +3,18 @@ class TweetSearchesController < ApplicationController
     def new
         @tweet_search = TweetSearch.new
         @user_ids = Tweet.uniq.pluck(:user_id)
-        
-        
     end
 
     def create
         @tweet_search = TweetSearch.create(tweet_search_params)
-        redirect_to @tweet_search
+           respond_to do |format|
+            if @tweet_search.save
+            format.js 
+          else
+            format.html { redirect_to @tweet_search, notice: "there was a problem." }
+            format.js
+            end  
+        end
     end
 
     def show
